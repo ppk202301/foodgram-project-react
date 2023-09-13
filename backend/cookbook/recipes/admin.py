790008815c98709f3c45827293d5850fc3f6f0a2
Pavel Prunev <pruneyv@yandex.ru>
2023-09-14 00:10:37 +0300

    Add models in Recipe app. Add admin panals description for them

--- backend/cookbook/recipes/admin.py
+++ backend/cookbook/recipes/admin.py
@@ -1,3 +1,186 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Cart,
+    Favorite,
+    Ingredient,
+    Ingredient_Recipe,
+    Recipe,
+    Recipe_Tag,
+    Tag
+)
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    """Admin panel for Cart model."""
+    list_display = (
+        'user', 
+        'recipe'
+    )
+    list_editable = (
+        'recipe',
+    )
+    list_filter = (
+        'user',
+        'recipe'
+    )
+    search_fields = (
+        'user',
+        'recipe'
+    )
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    """Admin panel for Favotite model."""
+    list_display = (
+        'user', 
+        'recipe'
+    )
+    list_editable = (
+        'recipe',
+    )
+    list_filter = (
+        'user',
+        'recipe'
+    )
+    search_fields = (
+        'user',
+        'recipe'
+    )
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    """Admin panel for Ingredient model."""
+    list_display = (
+        'name',
+        'unit'
+    )
+    list_editable = (
+        'unit',
+    )
+    list_filter = (
+        'name',
+        'unit'
+    )
+    search_fields = (
+        'name',
+        'unit'
+    )
+
+
+class IngredientInline(admin.TabularInline):
+    """Admin panel for Ingredient in Recipe model."""
+    model = Ingredient_Recipe
+    list_display = (
+        'recipe',
+        'ingredient',
+        'quantity'
+    )
+    list_editable = (
+        'recipe',
+        'ingredient',
+        'quantity'
+    )
+    list_filter = (
+        'recipe',
+        'ingredient',
+    )
+    search_fields = (
+        'recipe',
+        'ingredient',
+        'quantity'
+    )
+
+
+class TagInLine(admin.TabularInline):
+    """Admin panel for Recipe Tags model."""
+    model = Recipe_Tag
+    list_display = (
+        'recipe',
+        'tag',
+        'author',
+        'reg_date'
+    )
+    list_editable = (
+        'recipe',
+        'tag',
+        'author',
+        'reg_date'
+    )
+    list_filter = (
+        'recipe',
+        'tag',
+        'author',
+        'reg_date'
+    )
+    search_fields = (
+        'recipe',
+        'tag',
+        'author',
+        'reg_date'
+    )
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    """Admin panel for Recipe model."""
+    inlines = (IngredientInline, TagInLine)
+    list_display = (
+        'name',
+        'about',
+        'author',
+        'pub_date',
+        'production_time',
+        'favorite_count',
+    )
+    list_editable = (
+        'about',
+        'production_time',
+    )
+    list_filter = (
+        'name',
+        'author',
+        'pub_date',
+        'production_time',
+    )
+    search_fields = (
+        'name',
+        'about',
+        'author',
+        'pub_date',
+        'production_time',
+    )
+    readonly_fields = (
+        'favorite_count',
+    )
+
+    @admin.display(description='Favorites')
+    def favorite_count(self, obj):
+        return obj.favorites.count()
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """Admin panel for Tag model."""
+    list_display = (
+        'name',
+        'color',
+        'slug'
+    )
+    list_editable = (
+        'color',
+        'slug'
+    )
+    list_filter = (
+        'name',
+        'color',
+        'slug'
+    )
+    search_fields = (
+        'name',
+        'color',
+        'slug'
+    )
--- backend/cookbook/recipes/models.py
+++ backend/cookbook/recipes/models.py
@@ -1,3 +1,272 @@
+from colorfield.fields import ColorField
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+MIN_AMOUNT = 0.001
+
+
+class Ingredient(models.Model):
+    """Ingredient model"""
+    name = models.CharField(
+        max_length=250,
+        verbose_name='ingredient name',
+        help_text='Could not be empty.'
+    )
+    unit = models.CharField(
+        max_length=30,
+        verbose_name='ingredient unit',
+        help_text='Could not be empty.'
+    )
+
+    class Meta:
+        verbose_name = 'Ingredient'
+        verbose_name_plural = 'Ingredients'
+        ordering = ['name']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'unit'],
+                name='unique_ingredient'
+            )
+        ]
+
+    def __str__(self):
+        return f'Ingredient: {self.name}, unit: {self.unit}'
+
+
+class Tag(models.Model):
+    """Tag model."""
+    name = models.CharField(
+        max_length=250,
+        unique=True,
+        verbose_name='Tag name',
+        help_text='Could not be empty. Must be unique.'
+    )
+    color = ColorField(
+        max_length=7,
+        unique=True,
+        verbose_name='Tag color HEX code.',
+        help_text='Could not be empty. Must be unique.'
+    )
+    slug = models.SlugField(
+        max_length=250,
+        unique=True,
+        verbose_name='Tag slag.',
+        help_text='Could not be empty. Must be unique.'
+    )
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+        ordering = ['name']
+
+    def __str__(self):
+        return f'Tag: {self.name}'
+
+
+class Recipe(models.Model):
+    """Recipe model."""
+    name = models.CharField(
+        max_length=250,
+        unique=True,
+        verbose_name='Recipe title',
+        help_text='Name your recipe.'
+    )
+    image = models.ImageField(
+        upload_to='recipes/media',
+        verbose_name='Recipe image',
+        help_text='Show the final result.'
+    )
+    about = models.TextField(
+        max_length=250,
+        blank=True,
+        verbose_name='Description',
+        help_text='Describe your recipe.'
+    )
+    production_time = models.PositiveIntegerField(
+        default=5,
+        verbose_name='Recipe production time',
+        help_text='Estimate approximate production time in minutes (min 5).',
+        validators=[
+            MinValueValidator(
+                5, 
+                'Production time should be more than 5 min.'
+            )
+        ]
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Date of publication'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Author',
+        related_name='recipes',
+        help_text='Point the recipe author.'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='Ingredient_Recipe',
+        verbose_name='Recipe ingregiemts',
+        help_text='Add all components to your dish.'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        through='Recipe_Tag',
+        verbose_name='Recipe tags',
+        help_text='Add all tags to your recipe.'
+    )
+
+    class Meta:
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return f'Recipe: {self.name}, author: {self.author}'
+
+
+class Cart(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='carts',
+        verbose_name='Recipe in the cart',
+        help_text='Point the ordered recipe.'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='buyers',
+        verbose_name='Buyer',
+        help_text='Point the buyer.'
+    )
+
+    class Meta:
+        verbose_name = 'Cart'
+        verbose_name_plural = 'Carts'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'user'],
+                name='no_dublicated_recipes_in_cart'
+            )
+        ]
+
+
+class Favorite(models.Model):
+    """User favorite recipes model."""
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+        verbose_name='Favorite recipe',
+        help_text='Point a favorite recipe.'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='users',
+        verbose_name='User who has the favorite recipe',
+        help_text='Point the recipe fan.'
+    )
+
+    class Meta:
+        verbose_name = 'Favorite'
+        verbose_name_plural = 'Favorites'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'user'],
+                name='no_dublicated_favorite_pairs'
+            )
+        ]
+
+
+class Ingredient_Recipe(models.Model):
+    """Ingredient in Recipe model."""
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='ingredients',
+        verbose_name='Ingredient',
+        help_text='One of recipe ingredients.'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Recipe',
+        help_text='Ingredient is a part of this recipe.'
+    )
+    quantity = models.FloatField(
+        default=MIN_AMOUNT,
+        verbose_name='quantity',
+        help_text='Ingredient amount in the recipe (min 0.001).',
+        validators=[
+            MinValueValidator(
+                MIN_AMOUNT,
+                'Min value should be more than 0.001.'
+            ),
+        ]
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'ingredient'],
+                name='no_duplicated_ingredients_in_recipe'
+            )
+        ]
+
+    def __str__(self):
+        return (f'Recipe {self.recipe.name} has '
+                f'ingredient {self.ingredient.name} in '
+                f'amount {self.amount} of '
+                f'{self.ingredient.measurement_unit}')
+
+
+class Recipe_Tag(models.Model):
+    """Tags of Recipe model."""
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipes_tags',
+        verbose_name='Recipe',
+        help_text='One of recipes.'
+    )
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE,
+        related_name='tags_recipes',
+        verbose_name='Tag',
+        help_text='One of recipe tags.'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='author',
+        verbose_name='Author',
+        help_text='Point the user who added the tag to the recipe.'
+    )
+    reg_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Date of registration'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe', 'tag'],
+                name='no _duplicated_tags_for_recipe'
+            )
+        ]
+        ordering = ['-reg_date']
+
+    def __str__(self):
+        return (f'Recipe {self.recipe.name} has '
+                f'tag {self.tag.name} '
+                f'added by {self.author.name} on '
+                f'{self.reg_date}')
