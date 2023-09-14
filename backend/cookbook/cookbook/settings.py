2aa3c514e2e156c9508ccfef2d940199aed6b327
Pavel Prunev <pruneyv@yandex.ru>
2023-09-14 03:15:24 +0300

    Correct settings for model in recipe app

--- backend/cookbook/cookbook/settings.py
+++ backend/cookbook/cookbook/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'colorfield',
     'users.apps.UsersConfig',
     'recipes.apps.RecipesConfig',
     'api.apps.ApiConfig',
